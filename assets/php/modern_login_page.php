21c45fb52620e3419a37f37aee60acb25050256d
Thomas Vita <Thomasvitacell@gmail.com>
2024-05-19 00:06:49 +0200

    implementazione pagina login/registrazione avanzata

---
+++ assets/php/modern_login_page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../css/style_login_page.css">
+    <title>Modern Login Page | AsmrProg</title>
+</head>
+
+<body>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Crea un Account</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>o usa la tua email per la registrazione</span>
+                <input type="text" placeholder="Nome">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <button>Registrati</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form>
+                <h1>Accedi</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>o usa la tua email</span>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <a href="recupero_password.php">Password dimenticata?</a>
+                <button>Accedi</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Bentornato!</h1>
+                    <p>Se hai già un account ti basterà fare l'accesso premendo il pulsante qui sotto</p>
+                    <button class="hidden" id="login">Accedi</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Benvenuto!</h1>
+                    <p>Registrati con i tuoi dati personali per sbloccare tutte le funzionalità del sito</p>
+                    <button class="hidden" id="register">Registrati</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="../js/login.js"></script>
+</body>
+
+</html>
