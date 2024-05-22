bf2e263fcde1c26be3ed1f82a9d261cdf2074efa
Thomas Vita <Thomasvitacell@gmail.com>
2024-05-22 19:16:12 +0200

    email sender finito ma non funzionante

--- assets/py/email_sender.py
+++ assets/py/email_sender.py
@@ -1,10 +1,21 @@
 import smtplib
+import json
+import sys
+
+# Leggi il percorso del file temporaneo passato come argomento
+data_file = sys.argv[1]
+
+# Leggi i dati dal file
+with open(data_file, 'r') as file:
+    data = json.load(file)
+
+userName = data['userName']
+products = data['products']
 
 email = "bottegadigerosa@gmail.com"
 receiver_email = "bottegadigerosa@gmail.com"
-subject = "ciao"
-message = "pappa"
-
+subject = userName
+message = "\n".join([f"{p['nome']} - {p['quantita']} pcs - €{p['prezzo_totale']}" for p in products])
 
 text = f"Subject: {subject}\n\n{message}"
 
