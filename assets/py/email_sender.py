326f5cd6036d00d68828190133d1fdb16a5c26e7
Thomas Vita <Thomasvitacell@gmail.com>
2024-05-22 19:03:55 +0200

    email sender funzionante ma non ha ancora accesso ai dati del carrello

--- assets/py/email_sender.py
+++ assets/py/email_sender.py
@@ -1,18 +1,18 @@
 import smtplib
 
-email = input("SENDER EMAIL: ")
-receiver_email = input("RECEIVER EMAIL: ")
-subject = input("SUBJECT: ")
-message = input("MESSAGE: ")
+email = "bottegadigerosa@gmail.com"
+receiver_email = "bottegadigerosa@gmail.com"
+subject = "ciao"
+message = "pappa"
 
 
 text = f"Subject: {subject}\n\n{message}"
 
-server = smtplib.SMTP("smtp.google.com", 587)
+server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
 server.login(email, "cmbt wfhi zfjl ockh")
 
 server.sendmail(email, receiver_email, text)
 
-print("Email has been sent to" + receiver_email)
+print("Email has been sent to " + receiver_email)
